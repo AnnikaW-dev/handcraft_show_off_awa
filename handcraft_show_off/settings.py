af222c874775f6dcaceacd33e30ff3b0bc0769a0
AnnikaW-dev <warmannika@gmail.com>
2025-05-13 13:38:22 +0200

    fix problem with css styling & Build Post Model

--- handcraft_show_off/settings.py
+++ handcraft_show_off/settings.py
@@ -87,6 +87,11 @@ DATABASES = {
     }
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com"
+]
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
@@ -123,6 +128,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from .views import Index
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('', Index.as_view(), name='home')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
