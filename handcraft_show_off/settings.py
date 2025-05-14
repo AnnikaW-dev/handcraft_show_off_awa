d09bc56e88069ee1662c122b00b5925bfda998db
AnnikaW-dev <warmannika@gmail.com>
2025-05-14 10:19:20 +0200

    Add Comment model

--- handcraft_show_off/settings.py
+++ handcraft_show_off/settings.py
@@ -18,17 +18,15 @@ if os.path.isfile('env.py'):
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-if os.path.exists('env.py'):
-    import env
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u%5$@10**2mev08)*z64p(w+xm-(!g5*+m7qt=e@!m177(j$qu'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
